eeb295d1913d082074043c8c6d33546346014427
kilyfa1 <abdulaziz77771a@gmail.com>
2023-10-26 06:22:40 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Login(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  const Login({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Login> createState() => _LoginForm();
+}
+
+class _LoginForm extends State<Login> {
+  final formKey = GlobalKey<FormState>();
+  TextEditingController userController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  var pasToggle = true;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ),
+        body: SingleChildScrollView(
+            child: Form(
+                key: formKey,
+                child: Column(children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 60.0),
+                    child: Center(
+                      child: Container(
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(25.0)),
+                        child: Image.asset('images/kistore.png'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 15.0, right: 15.0, top: 20.0, bottom: 0),
+                    child: TextFormField(
+                      controller: userController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Username',
+                          hintText: "Enter valid username in here"),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your Username';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 15.0, right: 15.0, top: 20.0, bottom: 0),
+                    child: TextFormField(
+                      obscureText: pasToggle,
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                          hintText: "Enter valid password in here",
+                          suffixIcon: InkWell(
+                              onTap: () {
+                                setState(() {
+                                  pasToggle = !pasToggle;
+                                });
+                              },
+                              child: Icon(pasToggle
+                                  ? Icons.visibility
+                                  : Icons.visibility_off))),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your Password ';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 0, right: 0, top: 10.0, bottom: 10.0),
+                      child: Text(
+                        'Forgot Password? click in here',
+                        style: TextStyle(color: Colors.red, fontSize: 13),
+                      ),
+                    ),
+                  ),
+                  if (formKey.currentState != null &&
+                      !formKey.currentState!.validate())
+                    Text(
+                      '',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  Container(
+                      height: 50,
+                      width: 250,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: TextButton(
+                          onPressed: () {
+                            if (formKey.currentState != null &&
+                                formKey.currentState!.validate()) {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) =>
+                                          MyHomePage(title: 'Homepage')));
+                            }
+                          },
+                          child: Text('Login',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 20)))),
+                  SizedBox(
+                    height: 130,
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text('New User? Create Account',
+                          style: TextStyle(color: Colors.blue)))
+                ]))));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
